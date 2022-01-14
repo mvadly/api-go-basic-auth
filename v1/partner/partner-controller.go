16777d998b0d655d516591577d954f7d5c63037c
mvadly <mfadly24@gmail.com>
2022-01-14 22:43:54 +0700

    set response to json

--- v1/partner/partner-controller.go
+++ v1/partner/partner-controller.go
@@ -3,5 +3,5 @@ package partner
 import "github.com/gin-gonic/gin"
 
 func GetDataKlaster(c *gin.Context) {
-	c.XML(200, gin.H{"status": true, "message": "ok"})
+	c.JSON(200, gin.H{"status": true, "message": "ok"})
 }
